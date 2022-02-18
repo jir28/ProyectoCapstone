d97faa6b095e3959b4ad27dce342c366e58a04b0
CarlosATG <87503410+CarlosATG@users.noreply.github.com>
2022-02-17 18:19:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import RPi.GPIO as GPIO  # puertos de raspberry
 import time
-from w1thermsensor import W1ThermSensor #libreria para sensor de temperatura de agua
+#from w1thermsensor import W1ThermSensor #libreria para sensor de temperatura de agua
 from yeelight import Bulb  # Libreria para foco MI
+from w1thermsensor import W1ThermSensor, Unit
+import numpy as np
 
-
-def send_data_idb(totlit, time_s):  # Función donde tenemos los datos para el envio de informacion a influxdb
-    bucket = "Rasp"
+def send_data_idb(totlit, time_s, temper):  # Función donde tenemos los datos para el envio de informacion a influxdb
+    bucket = "Raspi"
     org = "jirs28"
-    token = "X3tzSDVgoXJphnnzfqqX0kbS3lkJYgA-ygF0OtBDqZw7jVrP3iDq10HsWHwNHLt3Wmcj3b0Oxb3DE0rg03fr-g=="
+    token = "VksMePOqNhrLWmA29wyq7FTcHOKPO0eeMBYX0ILkwT2DgN6rWzmOeS8g8JVCX3wBgwu6py1ftSGLAYyt46A1Bg=="
     url = "https://us-east-1-1.aws.cloud2.influxdata.com"
 
     client = influxdb_client.InfluxDBClient(
@@ -21,8 +22,10 @@ def send_data_idb(totlit, time_s):  # Función donde tenemos los datos para el e
     write_api = client.write_api(write_options=SYNCHRONOUS)
     p = influxdb_client.Point("Litros por baño").tag("Zone", "baño").field("Litros", totlit)
     ptime = influxdb_client.Point("Tiempo por baño").tag("Zone", "baño").field("Segundos", time_s)
+    ptemp = influxdb_client.Point("Temperatura promedio").tag("Zone", "baño").field("Temperatura", temper)
     write_api.write(bucket=bucket, org=org, record=p)
     write_api.write(bucket=bucket, org=org, record=ptime)
+    write_api.write(bucket=bucket, org=org, record=ptemp)
     print("Data Sended")
 
 
@@ -53,7 +56,9 @@ global rate_cnt, tot_cnt, TotLit, LperM, tiempo, aux, bulb
 rate_cnt = 0
 tot_cnt = 0
 aux = 0
-bulb = Bulb("192.168.3.28")
+bulb = Bulb("192.168.100.96")
+temperaturas = []
+sensor = W1ThermSensor()
 
 
 def Pulse_cnt(inpt_pin):
@@ -70,7 +75,6 @@ if __name__ == '__main__':
     # MAIN
     print("Registro de litros gastados y temperatura promedio el dia: ", str(time.asctime(time.localtime(time.time()))))
     rpt_int = int(input("\nSegundos de captar el flujo "))
-    print("Reportes cada", rpt_int, " segundos")
     print("Control + c para salir")
     bulb.turn_on()  # encendemos el foco
     bulb.set_hsv(47, 0, 1)
@@ -80,6 +84,8 @@ if __name__ == '__main__':
     while (rpt_int < 10):  # rpt_int<10 or,,or GPIO.input(inpt)==True <-- esta linea para pruebas antes de todo lo demas
         rpt_int += 1
         TotLit = round(tot_cnt * constant, 2)
+        temperature_in_celsius = sensor.get_temperature()
+        temperaturas.append(temperature_in_celsius)
         if (TotLit >= 2 and aux == 0):
             bulb.set_rgb(243, 159, 24)
             bulb.set_brightness(100)
@@ -94,8 +100,6 @@ if __name__ == '__main__':
     print('Tiempo:  ', time_shower, ' segundos')
     GPIO.cleanup()
     bulb.turn_off()
-
-    send_data_idb(TotLit, time_shower)  # Llamar la función para enviar la cantidad de litros gastados
-    # send_data_idb(Temp) #AL tener el sensor se enviara la temperatura promedio o todos los valores(por determinar)
-
+    Temp = np.mean(temperaturas)
+    send_data_idb(TotLit, time_shower, Temp)  # Llamar la función para enviar la cantidad de litros gastados
     print("Se acabo")
