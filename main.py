9a6d53730acdacd29691ee357b5fd134abe98b67
CarlosATG <87503410+CarlosATG@users.noreply.github.com>
2022-03-08 16:10:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import sys
+from yeelight import Bulb
 import showerS
 
 
 if __name__ == '__main__':
+    bulb = Bulb("192.168.100.96")
+    bulb.turn_on()
     while True:
         tem = showerS.flujo()
         if(tem==5):
