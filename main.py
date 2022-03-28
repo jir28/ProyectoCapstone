4a7397bff348ab558752ceda63b476b739ad55f7
Jair Zamorano <87546129+jir28@users.noreply.github.com>
2022-03-28 13:04:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import showerS
 
 
 if __name__ == '__main__':
-    bulb = Bulb("192.168.100.96")
+    bulb = Bulb("ip de tu foco xiamoi yeelight")
     bulb.turn_on()
     while True:
         tem = showerS.flujo()
